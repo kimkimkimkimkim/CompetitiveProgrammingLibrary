f395408a8e9fdbac73abf1e55ea4e02ab1c55f67
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2020-01-24 18:29:15 +0900

    fix indent of PrimeFactorization.cs

--- PrimeFactorization.cs
+++ PrimeFactorization.cs
@@ -1,27 +1,27 @@
 static List<int[]> PrimeFactorization(int n){
 
-	    List<int[]> arr = new List<int[]>();
-	    int temp = n;
+    List<int[]> arr = new List<int[]>();
+    int temp = n;
 
-	    for(int i=2;i<=Math.Ceiling(Math.Sqrt(n));i++){
-	        if(temp%i==0){
-	            int count = 0;
-	            while(temp%i==0){
-	                count++;
-	                temp /= i;
-	            }
-	            arr.Add(new int[2]{i,count});
-	        }
-	    }
+    for(int i=2;i<=Math.Ceiling(Math.Sqrt(n));i++){
+        if(temp%i==0){
+            int count = 0;
+            while(temp%i==0){
+                count++;
+                temp /= i;
+            }
+            arr.Add(new int[2]{i,count});
+        }
+    }
 
-	    if(temp != 1){
-	        arr.Add(new int[2]{temp,1});
-	    }
+    if(temp != 1){
+        arr.Add(new int[2]{temp,1});
+    }
 
-	    if(arr.Count() == 0){
-	        arr.Add(new int[2]{n,1});
-	    }
+    if(arr.Count() == 0){
+        arr.Add(new int[2]{n,1});
+    }
 
 
-	    return arr;
-	}
+    return arr;
+}
